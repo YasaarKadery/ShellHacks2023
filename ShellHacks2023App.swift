48e890e58fb465d6c14f59caa8b73d8971e80e41
YasaarKadery <ykadery12@gmail.com>
2023-09-16 00:36:45 -0400

    init commit

---
+++ ShellHacks2023App.swift
@@ -0,0 +1,17 @@
+//
+//  ShellHacks2023App.swift
+//  ShellHacks2023
+//
+//  Created by Yasaar Kadery on 9/16/23.
+//
+
+import SwiftUI
+
+@main
+struct ShellHacks2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
