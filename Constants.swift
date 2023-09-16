edd6bc0ba333110ef08afa4186696a55fa847185
YasaarKadery <ykadery12@gmail.com>
2023-09-16 01:04:16 -0400

    added launch view

---
+++ Constants.swift
@@ -0,0 +1,26 @@
+//
+//  Constants.swift
+//  ShellHacks2023
+//
+//  Created by Yasaar Kadery on 9/16/23.
+//
+
+import Foundation
+import SwiftUI
+
+
+struct Constants {
+    static let TextBlack: Color = .black
+    static let TextWhite: Color = .white
+    static let Background: Color = Color(red: 0.1, green: 0.11, blue: 0.12)
+    static let SubheadingWhitishBlue: Color = Color(red: 0.82, green: 0.91, blue: 1)
+    static let Primary: Color = Color(red: 0, green: 0.52, blue: 1)
+    static let linearGradientBackground: LinearGradient = LinearGradient(
+        stops: [
+            Gradient.Stop(color: Color(red: 0.15, green: 0.32, blue: 0.74).opacity(0.8), location: 0.00),
+            Gradient.Stop(color: Color(red: 0.02, green: 0.05, blue: 0.13).opacity(0), location: 0.71),
+        ],
+        startPoint: UnitPoint(x: 0.5, y: 0),
+        endPoint: UnitPoint(x: 0.5, y: 1)
+    )
+}
--- ContentView.swift
+++ ContentView.swift
@@ -6,18 +6,100 @@
 //
 
 import SwiftUI
+import MapKit
+
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+            LaunchView()
     }
 }
+
+struct LaunchSubTextView: View {
+    var body: some View {
+        Text("Drive safer, understand better! NaviGuardian helps you improve your driving while demystifying car insurance for you.")
+          .font(Font.custom("Rubik", size: 16))
+          .kerning(1)
+          .foregroundColor(Constants.SubheadingWhitishBlue)
+          .frame(width: 305, height: 94, alignment: .leading)
+    }
+}
+struct StartJourneyButton: View {
+    var body: some View {
+        HStack(alignment: .center, spacing: 15) {
+            Text("START JOURNEY")
+            .font(
+            Font.custom("Poppins", size: 16)
+            .weight(.semibold)
+            )
+            .kerning(0.6)
+            .foregroundColor(.white)
+        }
+        .padding(.horizontal, 111)
+        .padding(.vertical, 20)
+        .background(Constants.Primary)
+        .cornerRadius(10)
+    }
+}
+struct LaunchView: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Constants.Background.ignoresSafeArea()
+                Constants.linearGradientBackground.ignoresSafeArea()
+                VStack {
+                    LaunchImageView()
+                    LaunchTextView()
+                    LaunchSubTextView()
+                    Spacer()
+                    NavigationLink {
+//                        StaySafeView()
+                    }label: {
+                        StartJourneyButton()
+                    }
+                }
+
+                }
+        }.tint(Color.white)
+            
+        
+    }
+}
+
+struct LaunchImageView: View {
+    var body: some View {
+        Rectangle()
+            .foregroundColor(.clear)
+            .frame(width: 414.12299, height: 282)
+        
+            .background(
+                Image("launch-car")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.horizontal, 24.12)
+                    .frame(width: 414.12298583984375, height: 282)
+                    .clipped()
+            )
+    }
+}
+
+struct LaunchTextView: View {
+    var body: some View {
+        VStack {
+            Text("WELCOME TO ")
+                .font(Font.custom("Rubik", size: 48).weight(.medium))
+                .kerning(0.6)
+                .foregroundColor(.white)
+            
+            Text("NaviGuardian!")
+                .font(Font.custom("Rubik", size: 48).weight(.medium))
+                .kerning(0.6)
+                .foregroundColor(.blue) // Change 'yourColor' to the desired color
+        }.frame(width: 387, height: 136, alignment: .center)
+    }
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
