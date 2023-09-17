40dff5aaa27b268e562d2286e2b924fae95158ab
YasaarKadery <ykadery12@gmail.com>
2023-09-16 22:51:02 -0400

    fixed the GPS view not loading properly

--- DashboardView.swift
+++ DashboardView.swift
@@ -10,57 +10,60 @@ import SwiftUI
 struct DashboardView: View {
     @EnvironmentObject var viewModel: AuthViewModel
     var body: some View {
-        ZStack {
-            Constants.Background.ignoresSafeArea()
-            if let user = viewModel.currentUser {
-                VStack {
-                    Image("car with bg 1")
-                    
-                      .kerning(0.14865)
-                      .foregroundColor(.white)
-                    Text("Welcome to ")
-                      .font(
-                        Font.custom("Rubik", size: 32)
-                          .weight(.bold)
-                      )
-                      .kerning(0.14865)
-                      .foregroundColor(.white)
-                    
-                    Text("Drive Assistant, \(user.fullname)")
-                      .font(
-                        Font.custom("Rubik", size: 32)
-                          .weight(.bold)
-                      )
-                      .kerning(0.14865)
-                      .foregroundColor(.white)
-                    Spacer()
-                    NavigationLink {
-                        SpeedView()
-                    } label: {
-                        ZStack {
-                            Rectangle()
-                              .foregroundColor(.clear)
-                              .frame(width: 358, height: 64)
-                              .background(Constants.Primary)
-                              .cornerRadius(10)
-                            HStack {
-                                Image(systemName: "steeringwheel")
-                                    .font(.largeTitle)
-                                    .foregroundColor(.white)
-                                    
-                                Text("START DRIVE")
-                                    .font(Font.custom("Rubik", size: 14))
-                                    .fontWeight(.bold)
-                                    .foregroundColor(.white)
-                            }
-                        }
+        NavigationView {
+            ZStack {
+                Constants.Background.ignoresSafeArea()
+                if let user = viewModel.currentUser {
+                    VStack {
+                        Image("car with bg 1")
                         
-                    }
+                          .kerning(0.14865)
+                          .foregroundColor(.white)
+                        Text("Welcome to ")
+                          .font(
+                            Font.custom("Rubik", size: 32)
+                              .weight(.bold)
+                          )
+                          .kerning(0.14865)
+                          .foregroundColor(.white)
+                        
+                        Text("Drive Assistant, \(user.fullname)")
+                          .font(
+                            Font.custom("Rubik", size: 32)
+                              .weight(.bold)
+                          )
+                          .kerning(0.14865)
+                          .foregroundColor(.white)
+                        Spacer()
+                        NavigationLink {
+                            SpeedView()
+                        } label: {
+                            ZStack {
+                                Rectangle()
+                                  .foregroundColor(.clear)
+                                  .frame(width: 358, height: 64)
+                                  .background(Constants.Primary)
+                                  .cornerRadius(10)
+                                HStack {
+                                    Image(systemName: "steeringwheel")
+                                        .font(.largeTitle)
+                                        .foregroundColor(.white)
+                                        
+                                    Text("START DRIVE")
+                                        .font(Font.custom("Rubik", size: 14))
+                                        .fontWeight(.bold)
+                                        .foregroundColor(.white)
+                                }
+                            }
+                            
+                        }
 
+                }
+                }
+                    
             }
-            }
-                
         }
+        
      
     }
 }
